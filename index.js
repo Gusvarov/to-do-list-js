8e05d18b6f9cd8e5c8f38a023e7c350f0e3831f4
Gusvarov <nazikgta24@gmail.com>
2019-11-07 15:04:16 +0000

    My Code

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
 const addToList = document.querySelector('.add-to-list');
 const textBox = document.querySelector('.text-box');
 const out = document.querySelector('.out');
+const outTask = document.querySelector('.out-task');
+
+
 
 addToList.addEventListener('click', () => {
-    const xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4 && xhr.status === 200) {
+
+    /*const xhr = new XMLHttpRequest();
+
+            const dataFromInput = textBox.value;
+            const dataToSend = {
+                item: dataFromInput,
+            };
             const clientData = JSON.parse(xhr.responseText);
-            out.innerHTML = clientData.name;
-        }
-    }
+            outTask.innerHTML = `<div>${cData.item}</div>`*/
+
+
+    const xhr = new XMLHttpRequest();
+
+    const dataFromInput = textBox.value;
+    const dataToSend = {
+        item: dataFromInput,
+    };
+
     xhr.open('POST', 'api/todo.json', true);
+
+    xhr.responseType = 'json';
+
+    xhr.setRequestHeader('Content-type', 'application/json');
+
     xhr.send();
+
+    // тело ответа {"сообщение": "Привет, мир!"}
+    xhr.onload = function() {
+    let responseObj = xhr.response;
+    alert(responseObj.name); // Привет, мир!
+    };
+
 })
+
